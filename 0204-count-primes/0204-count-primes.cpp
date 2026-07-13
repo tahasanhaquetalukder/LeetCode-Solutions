0d67ec95a2239c38da9b377caeba661b35611ad1
Tahasan Haque Talukder <tahasan5800p@gmail.com>
2026-07-13 17:07:25 +0600

    Time: 246 ms (14.39%), Space: 12.6 MB (64.68%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>isprime(n+1,true);
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            if(isprime[i])
+            {
+                count++;
+                for(int j=i*2;j<n;j=j+i)
+                {
+                    isprime[j]=false;
+                }
+                
+            }
+        }
+        return count;
+
+    }
+};
